911e07d76b590b3d13bb2f4859344c5c75173381
LuffyBySunny <Sunny4droid@gmail.com>
2017-09-20 20:19:55 +0800

    first commit

---
+++ app/src/main/java/com/example/droodsunny/thirddayfragment/News.java
@@ -0,0 +1,28 @@
+package com.example.droodsunny.thirddayfragment;
+
+/**
+ * Created by DroodSunny on 2017/9/16.
+ */
+
+public class News {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String title;
+    public String content;
+
+
+}
